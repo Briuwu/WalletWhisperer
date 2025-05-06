653e096179c51d2fb3e0571e03bcdd86eb4af903
Briuwu <millontebrian@gmail.com>
2025-05-06 16:26:58 +0800

    feat: replace AlertDialog with Dialog component in EndSession and update dependencies

--- app/chat/end-session.tsx
+++ app/chat/end-session.tsx
@@ -3,16 +3,15 @@ import { X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import Link from "next/link";
 
 export const EndSession = ({
@@ -25,8 +24,8 @@ export const EndSession = ({
   isPending: boolean;
 }) => {
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         <Button
           type="button"
           disabled={status !== "ready"}
@@ -35,25 +34,23 @@ export const EndSession = ({
         >
           <X /> end session
         </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            Are you sure you want to end this session?
-          </AlertDialogTitle>
-          <AlertDialogDescription>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to end this session?</DialogTitle>
+          <DialogDescription>
             You will not be able to continue this session.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel disabled={isPending} asChild>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button disabled={isPending} asChild variant="destructive">
             <Link href="/home"> go back (no save)</Link>
-          </AlertDialogCancel>
-          <AlertDialogAction onClick={handleEndSession} disabled={isPending}>
+          </Button>
+          <Button onClick={handleEndSession} disabled={isPending}>
             generate report
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
